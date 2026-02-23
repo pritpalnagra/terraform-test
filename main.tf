49747664644c6941a815aeed6bd039ad6b7e6ee8
Pritpal Singh Nagra <nagra.pritpal@gmail.com>
2026-02-22 19:40:54 -0500

    Initial terraform commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.61.0"
+    }
+  }
+  required_version = ">=1.9.0"
+}
+
+provider "azurerm"{
+  features{
+    
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "prit-rg"
+  location = "Canada Central"
+}
+
+
